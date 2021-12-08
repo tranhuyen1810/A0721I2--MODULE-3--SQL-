0196315a1022d3abb25650e4ef409d16f9dcadc3
unknown <huyen18102001@gmail.com>
2021-12-08 13:45:01 +0700

    upbai

---
+++ bai1_Cơ sở dữ liệu quan hệ/quanlysinhvien.sql
@@ -0,0 +1,16 @@
+create database quanlysinhvien
+go
+
+create table class (
+id int,
+name varchar(100)
+)
+
+create table teacher(
+id int,
+name varchar(100),
+age int,
+country varchar(100)
+)
+
+
